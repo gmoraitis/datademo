82407e58c5ee8c47fce85d6c6cd9dacaf91b06a2
gmoraitis <moraitis.georgios@icloud.com>
2020-06-02 21:58:15 +0300

    Update js file

--- js/rfm.js
+++ js/rfm.js
@@ -1,85 +1,129 @@
 
-  function readTable(path) {
+  function readTable(path, maxScore) {
   
     // read csv
     d3.csv(path, function (data) {
-      
-      // Change to numeric
-      changeNumeric(data);
-  
-      // Extract columns
-  
-      // Apply extractScores function to the columns above
-  
-      // Append columns to data
-  
-      // Create and render table
-  
+      main(data, maxScore);
     });
   
   };
-  
-  // Change to numeric
-  function changeNumeric(data) {
-    data.forEach(function (col) {
-      col.RECENCY = +col.RECENCY;
-      col.FREQUENCY = +col.FREQUENCY;
-      col.MONETARY = +col.MONETARY;
-    });
-  };
-  
-  
-  // Extract scores
-  function extractScores(column, maxScore) {
-  
-    // Get min and max
-    min = Math.min(...column);
-    max = Math.max(...column);
-  
-    // Calculate step
-    step = (max - min) / maxScore
+
+  /**
+ * Foo takes any argument.
+ * The return value is 'baz' in all cases.
+ * @param {*} bar - Any argument
+ * @param {string} [optionalArg] - An optional argument that is a string
+ */
+  function main(data, maxScore) {
     
-    // Populate scores
-    scores = []
-    column.forEach(function (el) {
-      score = 0;
-      do {
-        score++;
-      }
-      while (el > min + score * step);
-      scores.push(score);
-    });
-    
-    return scores;
-  }
+    // Change to numeric
+    changeNumeric(data);
   
-  extractScores([2, 45, 23, 2, 1, 76, 34, 22, 15], 5);
-  // [1, 6, 3, 1, 1, 10, 5, 3, 2]
-   
+    // Extract columns
+    let recency = extractColumn(data, 'RECENCY');
+    let frequency = extractColumn(data, 'FREQUENCY');
+    let monetary = extractColumn(data, 'MONETARY');
+
+    // Apply extractScores function to the columns above
+    let rs = extractScores(recency, maxScore);
+    let fs = extractScores(frequency, maxScore);
+    let ms = extractScores(monetary, maxScore);
+    let labels = []
+    for (let i = 0; i < data.length; i++)
+      labels.push([rs[i], fs[i], ms[i]])
+    console.log(labels);
+
+    // Append columns to data
+
+    // Create and render table
+};
   
+/**
+ * Foo takes any argument.
+ * The return value is 'baz' in all cases.
+ * @param {*} bar - Any argument
+ * @param {string} [optionalArg] - An optional argument that is a string
+ */
+function changeNumeric(data) {
+  data.forEach(function (row) {
+    row.RECENCY = +row.RECENCY;
+    row.FREQUENCY = +row.FREQUENCY;
+    row.MONETARY = +row.MONETARY;
+  });
+};
+
+/**
+ * Foo takes any argument.
+ * The return value is 'baz' in all cases.
+ * @param {*} bar - Any argument
+ * @param {string} [optionalArg] - An optional argument that is a string
+ */
+function extractColumn(data, colName) {
+  column = [];
+  data.forEach(function (row) {
+    column.push(row[colName]);
+  });
+  return column;
+} 
+
+/**
+ * Foo takes any argument.
+ * The return value is 'baz' in all cases.
+ * @param {*} bar - Any argument
+ * @param {string} [optionalArg] - An optional argument that is a string
+ */
+function extractScores(column, maxScore) {
+
+  // Get min and max
+  min = Math.min(...column);
+  max = Math.max(...column);
+
+  // Calculate step
+  step = (max - min) / maxScore
   
+  // Populate scores
+  scores = []
+  column.forEach(function (el) {
+    score = 0;
+    do {
+      score++;
+    }
+    while (el > min + score * step);
+    scores.push(score);
+  });
   
-  
-  
-  // Create table
-  function update(data, columns) {
-    var table = d3.select('table').append('table')
-    var thead = table.append('thead')
-    var tbody = table.append('tbody')
-    thead.append('tr').selectAll('th').data(columns).enter().append('th').text(function (d) { return d })
-    var rows = tbody.selectAll('tr').data(data).enter().append('tr')
-    var cells = rows.selectAll('td').data(function (row) {
-      return columns.map(function (column) {
-        return { column: column, value: row[column] }
-      })
-    }).enter().append('td').text(function (d) { return d.value })
-    return table;
-  };
-  
-  // Clear table
-  function clear() {
-  var table = d3.select('table').selectAll("table").remove();
-  };  
+  return scores;
+}  
+ 
+/**
+ * Foo takes any argument.
+ * The return value is 'baz' in all cases.
+ * @param {*} bar - Any argument
+ * @param {string} [optionalArg] - An optional argument that is a string
+ */
+function update(data, columns) {
+  var table = d3.select('table').append('table')
+  var thead = table.append('thead')
+  var tbody = table.append('tbody')
+  thead.append('tr').selectAll('th').data(columns).enter().append('th').text(function (d) { return d })
+  var rows = tbody.selectAll('tr').data(data).enter().append('tr')
+  var cells = rows.selectAll('td').data(function (row) {
+    return columns.map(function (column) {
+      return { column: column, value: row[column] }
+    })
+  }).enter().append('td').text(function (d) { return d.value })
+  return table;
+};
+
+/**
+ * Foo takes any argument.
+ * The return value is 'baz' in all cases.
+ * @param {*} bar - Any argument
+ * @param {string} [optionalArg] - An optional argument that is a string
+ */
+function clear() {
+var table = d3.select('table').selectAll("table").remove();
+};  
     
     
   
