087302fbd9e70dcb15629c2c785d7ea67df2ffa2
gmoraitis <moraitis.georgios@icloud.com>
2020-06-04 11:23:45 +0300

    Refactored for simple presentation

--- js/rfm.js
+++ js/rfm.js
@@ -2,17 +2,18 @@
   function readTable(path, maxScore) {
   
     // read csv
-    d3.csv(path, function (data) {
+    d3.csv("/files/rfm.csv", function (data) {
       main(data, maxScore);
     });
   
   };
 
   /**
- * Foo takes any argument.
- * The return value is 'baz' in all cases.
- * @param {*} bar - Any argument
- * @param {string} [optionalArg] - An optional argument that is a string
+ * Main takes two arguments.Data which is the csv and a max score for the calculation of the data.
+ * The return value is data ready to be render in d3 as perhaps a table .
+ * Main uses the following functions a)changeNumeric, b)extractColumn, c)extractScores.  
+ * @param {*} data - The csv file.
+ * @param {*} maxScore - A given number from user to have an end in our calculation.
  */
   function main(data, maxScore) {
     
@@ -36,13 +37,13 @@
     // Append columns to data
 
     // Create and render table
+    update(data,labels);
+    
 };
   
 /**
- * Foo takes any argument.
- * The return value is 'baz' in all cases.
- * @param {*} bar - Any argument
- * @param {string} [optionalArg] - An optional argument that is a string
+ * Converts a string to number for the calculations.
+ * With the  + operator we can do the change . 
  */
 function changeNumeric(data) {
   data.forEach(function (row) {
@@ -53,10 +54,11 @@ function changeNumeric(data) {
 };
 
 /**
- * Foo takes any argument.
- * The return value is 'baz' in all cases.
- * @param {*} bar - Any argument
- * @param {string} [optionalArg] - An optional argument that is a string
+ * It takes two arguments.
+ * The return value is 'column' in all cases.
+ * @param {*} data - Is the
+ * @param {*} colName - An optional argument that is a string.
+ * @param {*} column - Is an array to be filled.
  */
 function extractColumn(data, colName) {
   column = [];
