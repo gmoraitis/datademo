7ac586712b8d349a12a58cc0928c85b66f803322
gmoraitis <moraitis.georgios@icloud.com>
2020-07-04 14:42:17 +0300

    Fixing testing.js

--- js/testing.js
+++ js/testing.js
@@ -1,8 +1,16 @@
+
+
+
+let maxScore = 10;
+
 //FOR MONETARY
-function readTable(maxScore) {
+function readTable() {
   
+  let path = document.getElementById('csv').value;
+  console.log(path);
+
   // read csv
-  d3.csv('/files/rfm.csv').then(function (data) {
+  d3.csv(path).then(function (data) {
     main(data, maxScore);
   });
 
@@ -24,72 +32,72 @@ function main(data, maxScore) {
   let recency = extractColumn(data, 'RECENCY');
   let frequency = extractColumn(data, 'FREQUENCY');
   let monetary = extractColumn(data, 'MONETARY');
- 
+
 
   // Apply calculateScores function to the columns above
   let recScores = calculateScores(recency, maxScore);
   let freqScores = calculateScores(frequency, maxScore);
-  let monScores= calculateScores(monetary, maxScore);
+  let monScores = calculateScores(monetary, maxScore);
   let labels = []
   let scoresCols = ['RecencyScores', 'FrequencyScores', 'MonetaryScores'];
-  for (let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++) {
     let scoresRow = {};
     scoresRow[scoresCols[0]] = recScores[i];
     scoresRow[scoresCols[1]] = freqScores[i];
     scoresRow[scoresCols[2]] = monScores[i];
     labels.push(scoresRow);
   }
-  console.log(data);
+  
   data = _.merge(data, labels);
   data.columns = data.columns.concat(scoresCols);
-  console.log(data);
-
-
-// Set the matgin of the svg
-var margin = {top: 50, right: 50, bottom: 50, left: 50};
-
-//set the width and height using the current width and height of ther div
-var width = 600 - margin.left - margin.right;
-var height = 600 - margin.top - margin.bottom;
-
-//create svg and append to visualization div
-var svg = d3.select("#visualization").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-
-// set the ranges
-var x = d3.scaleBand()
-  .range([0, width]);
-
-var y = d3.scaleLinear()
-  .range([height, 0]);
-
-    // Scale the range of the data in the domains
-    x.domain(data.map(function(row) { return row.ID; }));
-    y.domain([0, d3.max(data, function(row) { return row.MONETARY; })]);
-    
   
-    // append the rectangles for the bar chart
+
+
+  // Set the matgin of the svg
+  var margin = { top: 50, right: 50, bottom: 50, left: 50 };
+
+  //set the width and height using the current width and height of ther div
+  var width = 600 - margin.left - margin.right;
+  var height = 600 - margin.top - margin.bottom;
+
+  //create svg and append to visualization div
+  var svg = d3.select("#visualization").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+  // set the ranges
+  var x = d3.scaleBand()
+    .range([0, width]);
+
+  var y = d3.scaleLinear()
+    .range([height, 0]);
+
+  // Scale the range of the data in the domains
+  x.domain(data.map(function (row) { return row.ID; }));
+  y.domain([0, d3.max(data, function (row) { return row.MONETARY; })]);
+
+
+  // append the rectangles for the bar chart
   svg.selectAll(".bar")
-  .data(data)
-  .enter().append("rect")
-  .attr("class", "bar")
-  .attr("x", function(row) { return x(row.ID); })
-  .attr("width", x.bandwidth())
-  .attr("y", function(row) { return y(row.MONETARY); })
-  .attr("height", function(row) { return height - y(row.MONETARY); });
-  
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function (row) { return x(row.ID); })
+    .attr("width", x.bandwidth())
+    .attr("y", function (row) { return y(row.MONETARY); })
+    .attr("height", function (row) { return height - y(row.MONETARY); });
+
   //add the x axis
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
-  
+
   //add the y axis
   svg.append("g")
     .call(d3.axisLeft(y));
-       
+
 };
 
 /**
@@ -98,12 +106,12 @@ var y = d3.scaleLinear()
 */
 function changeNumeric(data) {
 
-data.forEach(function (row) {
-  row.RECENCY = +row.RECENCY;
-  row.FREQUENCY = +row.FREQUENCY;
-  row.MONETARY = +row.MONETARY;
-  row.id = +row.ID;
-});
+  data.forEach(function (row) {
+    row.RECENCY = +row.RECENCY;
+    row.FREQUENCY = +row.FREQUENCY;
+    row.MONETARY = +row.MONETARY;
+    row.id = +row.ID;
+  });
 };
 
 /**
@@ -114,12 +122,12 @@ data.forEach(function (row) {
 * @param {*} column - Is an array to be filled.
 */
 function extractColumn(data, colName) {
-column = [];
-data.forEach(function (row) {
-  column.push(row[colName]);
-});
-return column;
-} 
+  column = [];
+  data.forEach(function (row) {
+    column.push(row[colName]);
+  });
+  return column;
+}
 
 /**
 * extractScores takes two arguments.
@@ -129,24 +137,28 @@ return column;
 */
 function calculateScores(column, maxScore) {
 
-// Get min and max
-min = Math.min(...column);
-max = Math.max(...column);
+  // Get min and max
+  min = Math.min(...column);
+  max = Math.max(...column);
 
-// Calculate step
-step = (max - min) / maxScore
+  // Calculate step
+  step = (max - min) / maxScore
 
-// Populate scores
-scores = []
-column.forEach(function (el) {
-  score = 0;
-  do {
-    score++;
-  }
-  while (el > min + score * step);
-  scores.push(score);
-});
+  // Populate scores
+  scores = []
+  column.forEach(function (el) {
+    score = 0;
+    do {
+      score++;
+    }
+    while (el > min + score * step);
+    scores.push(score);
+  });
 
-return scores;
+  return scores;
+}
+
+function clear(){
+  document.getElementsById('visualization').style.display = "none";
 }
 
